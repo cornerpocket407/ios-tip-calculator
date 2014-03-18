75631d6b6d2a533c9935c3e0b2475a7158092845
Tony Dao <tony.dao@Admins-MacBook-Pro.local>
2014-03-18 10:35:16 -0700

    Initial Commit

---
+++ rottentomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rottentomatoes
+//
+//  Created by Tony Dao on 3/18/14.
+//  Copyright (c) 2014 Tony Dao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
